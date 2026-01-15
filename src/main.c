30e84802740e02ec032552b75860b3c7465f59f0
coder-lescob <gabriel.lescob@outlook.com>
2026-01-15 18:38:41 +0100

    classify the token inputed per the user

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+
 #include "token.h"
 
 #define STR_LEN 100
@@ -8,9 +10,14 @@ int main(void) {
     char str[STR_LEN + 1]; // + the 0 terminator
     fgets(str, STR_LEN, stdin);
 
+    // remove the new line character
+    if (str[strlen(str) - 1] == '\n') {
+        str[strlen(str) - 1] = 0;
+    }
+
     // create a token to hold it
-    Token token = {.type = TOKEN_ILLEGAL, .word = str};
+    Token token = {.type = ClassifyToken(str), .word = str};
 
     // print it
-    printf("illegal token %s\n", str);
+    printf("token %s of type %d\n", token.word, token.type);
 }
